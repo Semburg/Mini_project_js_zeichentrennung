fc65362a862e9dd93144982e033a2c22fc71894c
Semburg <semburg.sb@gmail.com>
2021-02-17 17:56:24 +0100

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,12 +11,20 @@ function trennFunction() {
     let trennResult = trennItem.split(trennPoint)
     firstPart.innerHTML = "Vorderer Teil:";
     secondPart.innerHTML = "Hinterer Teil:";
+    firstPart.style.color = "black";
+    secondPart.style.color = "black";
+
 
     if (trennItem == 0 || trennItem == null, trennPoint == 0 || trennPoint == null){
-        alert("Please Fill All Required Field");
+        // alert("Please Fill All Required Field");
+        firstPart.style.color = "red";
+        secondPart.style.color = "red";
+        firstPart.innerHTML += " " + "Eingabe bei Eingabefeld und/oder Trennfeld fehlt!"
+        secondPart.innerHTML += " " + "Eingabe bei Eingabefeld und/oder Trennfeld fehlt!";
+
         return false;
     } else if (trennItem.includes(trennPoint)) {
-        firstPart.style.color = "black";
+        
         if (trennNach.checked) {
             firstPart.innerHTML += " " + trennResult[0] + trennPoint
             secondPart.innerHTML += " " + trennResult[1]
